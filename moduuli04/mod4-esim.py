9960492110958be20098d9bf05e13e5af01dd947
mattpe <mattpe@metropolia.fi>
2024-01-23 11:33:50 +0200

    add more module 4 examples

--- moduuli04/mod4-esim.py
+++ moduuli04/mod4-esim.py
@@ -25,3 +25,53 @@ print(f"i:n lopullinen arvo silmukan jälkeen: {i}")
 
 # voit testata ehtolauseiden toimintaa tulostamalla niiden arvoja
 print(i < i+1)  # -> True
+
+
+# sisäkkäiset kontrollirakenteet ja satunnaisuus
+import random
+
+random_number = random.randint(1, 3)
+print(f"Arvottu numero: {random_number}")
+
+# pelin "asetukset"
+player_count = 4
+dice_amount = 3
+
+# alustetaan parhaan tuloksen näyttämiseen tarvittavat muuttujat
+best_player = None
+best_result = 0
+
+# jokainen pelaaja suorittaa vuorollaan, aloitetaan pelaajasta 1
+current_player = 1
+while current_player <= player_count:
+    result = 0  # nopan silmälukujen summan ennen heittoja
+    throw_count = 0  # iteraattori nopan heitoille
+    # noppia heitetään dice_amount -muuttujassa asetettu määrä
+    while throw_count < dice_amount:
+        die = random.randint(1, 6)
+        print(f"Pelaaja {current_player}, {throw_count+1}. heitto: {die}")
+        result = result + die
+        throw_count += 1  # sama kuin throw_count = throw_count + 1
+    print(f"Pelaajan {current_player} tulos: {result}")
+    # testataan saatiinko uusi paras tulos, ja tallennetaan tarvittaessa edellisen
+    # parhaan tuloksen tilalle, päivitetään samalla paras pelaaja
+    if result > best_result:
+        best_result = result
+        best_player = f"Pelaaja {current_player}"
+    # jos tulos ei ole parempi, mutta on sama kuin edellinen paras tulos,
+    # yhdistetään pelaajan nimi edellisen parhaan pelajaan nimen lisäksi samaan stringiin
+    elif result == best_result:
+        best_player = best_player + f", Pelaaja {current_player}"
+    current_player = current_player + 1
+print(f"paras tulos {best_result}, {best_player}")
+
+
+# Break-komento, "heittää" ulos aktiivisesta silmukasta, suoritus jatkuu koodilohkon jälkeen
+counter = 0
+while True:  # ikuinen silmukka
+    print(f"laskuri eka {counter}")
+    counter += 1
+    if counter == 5:
+        break  # poistuu silmukan koodilohkosta samantien, allaoleva ei tulostu
+    print(f"laskuri toka {counter}")
+
