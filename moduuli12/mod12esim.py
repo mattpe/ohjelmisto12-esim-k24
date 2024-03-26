dfaf2995baecf698e6a19c91599c041ccbf0bb7d
mattpe <mattpe@metropolia.fi>
2024-03-26 11:13:44 +0200

    add request example

--- moduuli12/mod12esim.py
+++ moduuli12/mod12esim.py
@@ -4,17 +4,25 @@ import requests
 
 
 def search_shows(search_term):
-    response = requests.get(f"https://api.tvmaze.com/search/shows?q={search_term}")
-    shows = response.json()
-    # print(f"Ensimmäisen sarjan nimi {shows[0]['show']['name']}")
-    # Tulostetaan kaikki hakutulokset
-    print(f"\nOhjelmahaun tulokset hakusanalla {search_term}:")
-    for show in shows:
-        # haetaan genret sisäkkäisellä silmukalla
-        genres = ""
-        for genre in show['show']['genres']:
-            genres = genres + genre + " "
-        print(f"{show['show']['name']} ({genres}): {show['show']['url']}")
+    url = f"https://api.tvmaze.com/search/shows?q={search_term}"
+    try:
+        response = requests.get(url)
+        print(f"HTTP response status code: {response.status_code}")
+        shows = response.json()
+        # print(f"Ensimmäisen sarjan nimi {shows[0]['show']['name']}")
+        # Tulostetaan kaikki hakutulokset, jos http-pyyntö onnistui
+        if response.status_code == 200:
+            print(f"\nOhjelmahaun tulokset hakusanalla {search_term}:")
+            for show in shows:
+                # haetaan genret sisäkkäisellä silmukalla
+                genres = ""
+                for genre in show['show']['genres']:
+                    genres = genres + genre + " "
+                print(f"{show['show']['name']} ({genres}): {show['show']['url']}")
+    except requests.exceptions.RequestException as error:
+        print("HTTP-pyyntö meni pieleen. Ei verkkoyhteyttä palvelimeen.")
+        print(error)
+
 
 search_input = input("Anna hakusana: ")
 search_shows(search_input)
