a5855a1b7a37b0f3d8ba84302b0fc64f99fc514a
mattpe <mattpe@metropolia.fi>
2024-04-12 10:20:17 +0300

    add simple dom examples

---
+++ js-moduuli03/example-mod3.js
@@ -0,0 +1,33 @@
+
+function locationSuccess (location) {
+  console.log('Käyttäjä paikannettu', location);
+}
+
+function locationError (error) {
+  console.log('paikannus fail', error);
+}
+
+const locationOptions = {
+  timeout: 3000
+};
+
+navigator.geolocation.getCurrentPosition(locationSuccess, locationError, locationOptions);
+console.log('Moro!');
+
+// DOM-kikkailua
+
+const section2 = document.querySelectorAll('section')[1];
+const pElement = section2.querySelector('p');
+pElement.textContent = 'muokattu';
+
+const newP = document.createElement('p');
+newP.textContent = 'uusi kappale';
+section2.append(newP);
+newP.style = 'color: blue';
+
+const thirdSection = document.querySelector('#third-section');
+thirdSection.innerHTML = `
+    <h2>Kolmannen osion otsikko</h2>
+    <p>
+        Tässä taas tekstiä.
+    </p>`;
